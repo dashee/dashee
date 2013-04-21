6842b8873d58f8d0b0a4834bd88604fd5310d061
David Buttar <david.buttar@gmail.com>
2013-04-21 20:05:58 +0000

    Moving UDP server to separate class, need to figure out how to get the UI interacting with it.

--- Androneee_Remote/src/com/confusedbrowser/androneee_remote/MainActivity.java
+++ Androneee_Remote/src/com/confusedbrowser/androneee_remote/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends Activity {
 	
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 	InetAddress serverAddr;
+	UDP_thread connection;
 	TextView t;
 	
 	@Override
@@ -23,33 +24,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        Thread thread = new Thread(new Runnable(){
-            @Override
-            public void run() {
-                try {
-	       	         DatagramSocket clientSocket = new DatagramSocket();
-	    	         InetAddress IPAddress = InetAddress.getByName("54.246.151.139");
-	    	         byte[] receiveData = new byte[1024];
-	    	         String sentence = "Hello from Daves Galaxy Nexus\n";
-	    	         byte[] sendData = sentence.getBytes("US-ASCII");
-	    	         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2047);
-	    	         clientSocket.send(sendPacket);
-	    	         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-	    	         clientSocket.receive(receivePacket);
-	    	         String modifiedSentence = new String(receivePacket.getData());
-	    	         Log.d("shahmirj", "FROM SERVER:" + modifiedSentence);
-	    	         TextView t = new TextView(MainActivity.this); 
-	    	         t=(TextView)findViewById(R.id.server_message); 
-	    	         t.setText(modifiedSentence);
-	    	         
-	    	         clientSocket.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        thread.start(); 
+        connection = new UDP_thread(this, "54.246.151.139", 2047);
+        connection.start();
 
     }
 
---
+++ Androneee_Remote/src/com/confusedbrowser/androneee_remote/UDP_thread.java
@@ -0,0 +1,53 @@
+package com.confusedbrowser.androneee_remote;
+
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+import android.view.Menu;
+import android.content.Intent;
+import android.content.Context;
+import android.widget.EditText;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.*;
+import java.net.*;
+
+public class UDP_thread extends Thread {
+	
+	String ip_address;
+	int port_num;
+	Context main_context;
+
+	public UDP_thread(Context context, String ip, int port)
+	{
+		super();
+		ip_address = ip;
+		port_num = port;
+		main_context = context;
+	}
+	
+	public void run() 
+    {    			
+	 try {
+		 DatagramSocket clientSocket = new DatagramSocket();
+		 InetAddress IPAddress = InetAddress.getByName(ip_address);
+		 byte[] receiveData = new byte[1024];
+		 String sentence = "FU from Daves Galaxy Nexus\n";
+		 byte[] sendData = sentence.getBytes("US-ASCII");
+		 DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port_num);
+		 clientSocket.send(sendPacket);
+		 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+		 clientSocket.receive(receivePacket);
+		 String modifiedSentence = new String(receivePacket.getData());
+		 Log.d("shahmirj", "FROM SERVER:" + modifiedSentence);
+		 /*TextView t = new TextView(MainActivity.this); 
+		 t= main_context.findViewById(R.id.server_message); 
+		 t.setText(modifiedSentence);*/
+		 clientSocket.close();
+	 } catch (Exception e) {
+		 e.printStackTrace();
+	 }
+    }
+    
+}
