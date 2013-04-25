aaf6000f407551276d28e73f438e0b5688c5ccb4
Shahmir Javaid <shahmirj@gmail.com>
2013-04-25 22:45:38 +0000

    Added comments on the Server.cpp file

--- servocontroller/src/Server.cpp
+++ servocontroller/src/Server.cpp
@@ -1,5 +1,12 @@
 #include "Server.h"
 
+/**
+ * This function will initialize the @port value which is passed as a parameter
+ * it will also set the @client_in_length value to be the sizeof @client_in 
+ * and set the server_in to have all 0's as its value.
+ *
+ * @param (int)port - The value of the port
+ */
 Server::Server(int port)
 {
     this->port = port;
