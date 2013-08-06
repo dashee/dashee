7b573a3e3e0e691dce2c3c1afc411749b5e74fa3
David Buttar <david.buttar@gmail.com>
2013-08-06 22:51:17 +0000

    Better comments and the outer arc lines.

--- client/android/src/com/confusedbrowser/androneee_remote/DrawHud.java
+++ client/android/src/com/confusedbrowser/androneee_remote/DrawHud.java
@@ -1,39 +1,64 @@
 package com.confusedbrowser.androneee_remote;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.*;
-import android.graphics.PorterDuff.Mode;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 
+/**
+ * This is currently mapping out the HUD display
+ * as of Shahmir's hud.fw.png, it should draw a central steer arc
+ * which will move when the car is steering. To the left and right are gauges
+ * reflecting power and battery.
+ *
+ * @author David Buttar
+ * @author Shahmir Javaid
+ */
 public class DrawHud extends View
 {
     Context context;
-    
 
-    
+    /*
+     * Various paths to be defined onSizeChanged
+     */
     Path steerPath = new Path();
     Path[] powerPaths = new Path[12];
     Path[] batteryPaths = new Path[12];
+    Path powerOuterArc = new Path();
+    Path batteryOuterArc = new Path();
+    
+    // Steer arc and oval are at the center of the design
+    // other aspects are offset from it.
     float steerArcRadius;
     final RectF steerOval = new RectF();
     
-    
-    
+    /*
+     *  Track positions on the grid.
+     */
     float centerX = 0.0f;
     float centerY = 0.0f;
+    // Top and bottom of the gauge paths
+    double gaugeTopY;
+    double gaugeBottomY;
+    double gaugeBarHieght;
+    double gaugeBarGap;
     
-    final RectF oval2 = new RectF();
+    /*
+     * Values to be updated to reflect app state
+     */
     float tilt = 50.0f;
     float powerPerc = 0.0f;
     
+    /*
+     * Style elements go here
+     */
     Paint steerLine;
     Paint steerLineLock;
     Paint activePowerBar;
     Paint inactivePowerBar;
     Paint activeBatteryBar;
+    Paint powerArc;
+    Paint batteryArc;
     int lineColor = 0x333333;
     int lockColor = 0xD93600;
     int activeBarColor = 0xD96D00;
@@ -82,6 +107,20 @@ public class DrawHud extends View
         activeBatteryBar.setAlpha(255);
         activeBatteryBar.setStyle(Paint.Style.FILL);
         
+        powerArc = new Paint();
+        powerArc.setAntiAlias(true);
+        powerArc.setColor(0xD96D00);
+        powerArc.setAlpha(255);
+        powerArc.setStrokeWidth(3.0f);
+        powerArc.setStyle(Paint.Style.STROKE);
+        
+        batteryArc = new Paint();
+        batteryArc.setAntiAlias(true);
+        batteryArc.setColor(0xFFFFFF);
+        batteryArc.setAlpha(255);
+        batteryArc.setStrokeWidth(3.0f);
+        batteryArc.setStyle(Paint.Style.STROKE);
+
     }
     
     /**
@@ -90,11 +129,20 @@ public class DrawHud extends View
      * constantly in the draw.
      */
     protected void onSizeChanged (int w, int h, int oldw, int oldh){
+    	// Noticed it sometimes runs with 0 values, not sure why.
+    	if(w ==0 || h==0) return;
     	this.centerX = w/2;
     	this.centerY = h/2;
+    	this.steerArcRadius = (h - h*0.08f) / 2.0f;
+    	this.gaugeBottomY = h-Math.round(h*0.1);
+    	this.gaugeBarHieght = Math.round(h*0.053);
+    	this.gaugeBarGap = Math.round(h*0.015);
+    	this.gaugeTopY = gaugeBottomY - (12*this.gaugeBarHieght) -(11*this.gaugeBarGap);
     	this.buildSteerPath(w, h);
     	this.addGaugePath(w, h, this.powerPaths, true);
     	this.addGaugePath(w, h, this.batteryPaths, false);
+    	this.addLeftArc();
+    	this.addRightArc();
     }
     
     /*
@@ -102,19 +150,54 @@ public class DrawHud extends View
      */
     private void buildSteerPath(int w, int h){
     	if (w == 0 || h==0) return;
-    	steerArcRadius = (h - h*0.08f) / 2.0f;
-    	// No thought in angles below simply played around till it
-    	// looked ok.
+
     	float steerArcSweepAngle = 280.0f;
         float steerArcStartAngle = -180.0f;
 
-        float p1x = (w/2) - steerArcRadius;
-        float p1y = (h/2) - steerArcRadius;
-        float p2x = (w/2) + steerArcRadius;
-        float p2y = (h/2) + steerArcRadius;
+        float p1x = (w/2) - this.steerArcRadius;
+        float p1y = (h/2) - this.steerArcRadius;
+        float p2x = (w/2) + this.steerArcRadius;
+        float p2y = (h/2) + this.steerArcRadius;
         
         this.steerOval.set(p1x, p1y, p2x, p2y);
         this.steerPath.arcTo(this.steerOval, steerArcStartAngle, -steerArcSweepAngle, true);
+    }
+    
+    /*
+     * Power arc
+     */
+    private void addLeftArc(){
+    	final RectF outerOval = new RectF();
+    	float radius = this.steerArcRadius*1.393f;
+
+        float p1x = this.centerX - radius;
+        float p1y = this.centerY - radius;
+        float p2x = this.centerX + radius;
+        float p2y = this.centerY + radius;
+        
+        outerOval.set(p1x, p1y, p2x, p2y);
+        float[] outerArcParams = this.getArcParams(this.gaugeBottomY, this.gaugeTopY, radius, false, true);
+        Log.d("dashee", outerArcParams[0]+" , "+-outerArcParams[1]);
+        this.powerOuterArc.arcTo(outerOval, outerArcParams[0], -outerArcParams[1], true);
+    }
+    
+    
+    /*
+     * Battery arc
+     */
+    private void addRightArc(){
+    	final RectF outerOval = new RectF();
+    	float radius = this.steerArcRadius*1.393f;
+
+        float p1x = this.centerX - radius;
+        float p1y = this.centerY - radius;
+        float p2x = this.centerX + radius;
+        float p2y = this.centerY + radius;
+        
+        outerOval.set(p1x, p1y, p2x, p2y);
+        float[] outerArcParams = this.getArcParams(this.gaugeBottomY, this.gaugeTopY, radius, false, false);
+        Log.d("dashee", outerArcParams[0]+" , "+outerArcParams[1]);
+        this.batteryOuterArc.arcTo(outerOval, outerArcParams[0],-outerArcParams[1], true);
     }
     
     /*
@@ -125,14 +208,14 @@ public class DrawHud extends View
     	final RectF innerOval = new RectF();
         final RectF outerOval = new RectF();
     	float innerRadius = steerArcRadius + steerArcRadius*0.16f;
-    	float outerRadius = steerArcRadius + steerArcRadius*0.38f;
+    	float outerRadius = steerArcRadius + steerArcRadius*0.39f;
     	
     	//Log.d("dashee", "percHieght: "+h*0.1);
-    	double curY = h-Math.round(h*0.1);
+    	double curY = this.gaugeBottomY;
     	//Log.d("dashee", "rectSize: "+h*0.053);//38.0
-    	double rectHeight = Math.round(h*0.053);
+    	double rectHeight = this.gaugeBarHieght;
     	//Log.d("dashee", "gap: "+ Math.round(h*0.015));//11
-    	double gap =   Math.round(h*0.015);
+    	double gap = this.gaugeBarGap;
     	
     	float p1x = (w/2) - innerRadius;
         float p1y = (h/2) - innerRadius;
@@ -165,19 +248,27 @@ public class DrawHud extends View
         }
     }
     
-    public float[] getArcParams(double startY, double endY, float radius, boolean reverse, boolean leftHandSide){
+    /*
+     * Based on desired starY and endY and radius get the appropriate 'path.arcTo' parameters.
+     * Uses getCircleX to find the x coordinates that intersect the centered circle, and then
+     * the getAngle function calculated the start angle form the center. 
+     */
+    private float[] getArcParams(double startY, double endY, float radius, boolean reverse, boolean leftHandSide){
     	double startXCoor[] = getCircleX(startY, radius);
     	double endXCoor[] = getCircleX(endY, radius);
     	
     	int xcoordinateSide = (leftHandSide) ? 0 : 1;
-    	
-    	/*Log.d("dashee", "curY: "+startY);
-    	Log.d("dashee", "x: "+startXCoor);
-    	Log.d("dashee", "Recommended angle: "+this.getAngle((float) startXCoor, (float) startY));*/
         
     	float startAngle = (float) this.getAngle((float) startXCoor[xcoordinateSide], (float) startY);
         float endAngle = (float) this.getAngle((float) endXCoor[xcoordinateSide], (float) (endY));
+
         float sweepAngle = startAngle - endAngle;
+        // If start angle is in 4th quarter and endAngle is in 3rd quarter
+        // we will end up drawing almost a full circle want to avoid this
+        // and sweep closest distance.
+        if(startAngle < -270 && endAngle > -90){
+        	sweepAngle = (sweepAngle + 360);
+        }
         float[] returnArray = {startAngle, sweepAngle, (float) startXCoor[xcoordinateSide]};
         if(reverse){
         	returnArray[0] = endAngle;
@@ -187,20 +278,22 @@ public class DrawHud extends View
         return returnArray;
     }
     
-    public void onScreenStateChanged (int screenState){
-    	invalidate();
-    }
-    
-    public double[] getCircleX(double y, double r)
+    /*
+     * Calculated the x coordinate of a circle with radius r at the center of the display
+     * returns both coordinates as line will intersect 2 times.
+     */
+    private double[] getCircleX(double y, double r)
     {
         double x = Math.sqrt(Math.abs(Math.pow(r, 2) - Math.pow(y-this.centerY, 2))); 
-        //Log.d("dashee", "x1: "+Math.abs(x-this.centerX)+ " x2: "+Math.abs(-x-this.centerX));
         double[] returnArray = {Math.abs(x-this.centerX), Math.abs(-x-this.centerX)};
         return returnArray;
     }
    
-   
-   public double getAngle(float x, float y)
+    /*
+     * Calculate the angle from the center of the screen to x, y. Starting at 3 O'Clock.
+     * Always return a negative angle, e.g -90 at 12, -180 at 9, -270 at 6.
+     */
+    private double getAngle(float x, float y)
    {
        double angle1 = Math.atan2(this.centerY - this.centerY, this.centerX - (this.centerX + 10));
        double angle2 = Math.atan2(this.centerY - y, this.centerX - x);
@@ -208,17 +301,12 @@ public class DrawHud extends View
    }
     
     /**
-     * This function will draw our artificial horizon, which works at an opposing angle
-     * to that of the phone. The rotation is handled outside, but this function deals with
-     * drawing our horizon boxes just big enough so there are no white spaces
+     * Draw our hud paths and apply appropriate paints
      * 
      * @param Canvas canvas - The canvas object to draw on.
      */
     protected void onDraw(Canvas canvas)
     {
-        //super.onDraw(canvas);
-        
-        //Always clear the canvas, seems needed if you switch off the display
         
         //Mid line right
         canvas.drawLine(this.centerX+this.steerArcRadius-80, this.centerY, this.centerX+this.steerArcRadius, this.centerY, steerLine);
@@ -235,6 +323,7 @@ public class DrawHud extends View
         }
         canvas.restore();
         
+        /* Draw Power arcs */
         int count  = 0;
         int cutOff = Math.round(this.powerPerc*12);
     	while(count <=11){
@@ -246,11 +335,15 @@ public class DrawHud extends View
     		count++;
     	}
     	
+    	canvas.drawPath(this.powerOuterArc, powerArc);
+    	
     	count = 0;
     	while(count <=11){
     		canvas.drawPath(this.batteryPaths[count], activeBatteryBar);    		
     		count++;
     	}
+    	
+    	canvas.drawPath(this.batteryOuterArc, batteryArc);
     	
         invalidate();
     }
