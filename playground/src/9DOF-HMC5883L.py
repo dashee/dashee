0a62b15d703ef4179b8b0e34dde273cb29c6d9a5
Shahmir Javaid <shahmirj@gmail.com>
2013-12-11 23:33:22 +0000

    Two's compliment and formatting updates

--- playground/src/9DOF-HMC5883L.py
+++ playground/src/9DOF-HMC5883L.py
@@ -5,9 +5,11 @@ import time
 import math
 
 def twosToInt(val, len):
+ 
     # Convert twos compliment to integer
     if(val & (1 << len - 1)):
         val = val - (1<<len)
+
     return val
 
 bus = smbus.SMBus(1)
@@ -16,29 +18,34 @@ address = 0x1e
 #bus.write_byte_data(address, 0x00, 0x00)
 #bus.write_byte_data(address, 0x01, 0x00)
 bus.write_byte_data(address, 0x02, 0x00)
+
+# Important for setup
 time.sleep(0.01)
 
 while True:
 
-    bus.write_byte(address, 0x03)
+    bus.write_byte(address, 0x06)
 
+    # Get the values from the server
     valX = (bus.read_byte_data(address, 0x03) << 8) | bus.read_byte_data(address, 0x04)
     valY = (bus.read_byte_data(address, 0x05) << 8) | bus.read_byte_data(address, 0x06)
     valZ = (bus.read_byte_data(address, 0x07) << 8) | bus.read_byte_data(address, 0x08)
 
+    # Update the values to be of two compliemnt
     valX = twosToInt(valX, 16);
     valY = twosToInt(valY, 16);
     valZ = twosToInt(valZ, 16);
 
-    radians = math.atan2(round(valX * 1.3, 4), round(valY * 1.3, 4))
+    # Change valX and valY to radians
+    radians = math.atan2(valX, valY)
 
+    # Compensate for errors
     if radians < 0:
         radians += 2*math.pi
     elif radians > 2*math.pi:
         radians -= 2*math.pi
 
-    print "{0:-3f}".format(radians * (180/math.pi))
-
-#    print math.atan2(valX, valY);
+    # Print the value to the output
+    print "{0:-3f}".format(radians), "{0:-3f}".format(math.floor(radians * 180 / math.pi))
     
-    time.sleep(0.01)
+    time.sleep(0.02)
