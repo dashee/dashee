684f9b60e6232e2d5cd9261d334a0db9632c3774
Shahmir <shahmirj@gmail.com>
2013-12-18 01:44:13 +0000

    Working set of tests, Accelerometer and Coordinate

--- server/tests/Coordinate.cpp
+++ server/tests/Coordinate.cpp
@@ -25,21 +25,21 @@ void dashee::test::Coordinate::testValueConstructions()
 {
     // Positive Values
     dashee::Coordinate<int> c1(10,5,20);
-    CPPUNIT_ASSERT(this->coordinate->getX() == 10);
-    CPPUNIT_ASSERT(this->coordinate->getY() == 5);
-    CPPUNIT_ASSERT(this->coordinate->getZ() == 1);
+    CPPUNIT_ASSERT(c1.getX() == 10);
+    CPPUNIT_ASSERT(c1.getY() == 5);
+    CPPUNIT_ASSERT(c1.getZ() == 20);
     
     // Negative values
     dashee::Coordinate<int> c2(-10, -5, -20);
-    CPPUNIT_ASSERT(this->coordinate->getX() == -10);
-    CPPUNIT_ASSERT(this->coordinate->getY() == -5);
-    CPPUNIT_ASSERT(this->coordinate->getZ() == -20);
+    CPPUNIT_ASSERT(c2.getX() == -10);
+    CPPUNIT_ASSERT(c2.getY() == -5);
+    CPPUNIT_ASSERT(c2.getZ() == -20);
     
     // Mixed values
     dashee::Coordinate<int> c3(-10, 5, -20);
-    CPPUNIT_ASSERT(this->coordinate->getX() == -10);
-    CPPUNIT_ASSERT(this->coordinate->getY() == 5);
-    CPPUNIT_ASSERT(this->coordinate->getZ() == -20);
+    CPPUNIT_ASSERT(c3.getX() == -10);
+    CPPUNIT_ASSERT(c3.getY() == 5);
+    CPPUNIT_ASSERT(c3.getZ() == -20);
 }
 
 /**
@@ -47,7 +47,11 @@ void dashee::test::Coordinate::testValueConstructions()
  */
 void dashee::test::Coordinate::testSetAndGetX()
 {
-    CPPUNIT_ASSERT(false);
+    CPPUNIT_ASSERT(this->coordinate->getX() == 0.0f);
+    this->coordinate->setX(1.9f);
+    CPPUNIT_ASSERT(this->coordinate->getX() == 1.9f);
+    this->coordinate->setX(-21.9f);
+    CPPUNIT_ASSERT(this->coordinate->getX() == -21.9f);
 }
 
 /**
@@ -55,7 +59,11 @@ void dashee::test::Coordinate::testSetAndGetX()
  */
 void dashee::test::Coordinate::testSetAndGetY()
 {
-    CPPUNIT_ASSERT(false);
+    CPPUNIT_ASSERT(this->coordinate->getY() == 0.0f);
+    this->coordinate->setY(1.9f);
+    CPPUNIT_ASSERT(this->coordinate->getY() == 1.9f);
+    this->coordinate->setY(-21.9f);
+    CPPUNIT_ASSERT(this->coordinate->getY() == -21.9f);
 }
 
 /**
@@ -63,7 +71,11 @@ void dashee::test::Coordinate::testSetAndGetY()
  */
 void dashee::test::Coordinate::testSetAndGetZ()
 {
-    CPPUNIT_ASSERT(false);
+    CPPUNIT_ASSERT(this->coordinate->getZ() == 0.0f);
+    this->coordinate->setZ(1.9f);
+    CPPUNIT_ASSERT(this->coordinate->getZ() == 1.9f);
+    this->coordinate->setZ(-21.9f);
+    CPPUNIT_ASSERT(this->coordinate->getZ() == -21.9f);
 }
 
 /**
