11cb18d4c663e4a6bb0704c20b1bcf7a8d423342
Shahmir Javaid <shahmirj@gmail.com>
2013-12-19 00:42:35 +0000

    More comments as I understand the ADXL345

--- playground/src/9DOF-ADXL345.py
+++ playground/src/9DOF-ADXL345.py
@@ -49,16 +49,35 @@ def setBandwidthRate(bus, address, hz):
 # Set the measurement range for 10-bit readings
 #
 # @param bus to write
+# @param address of the bus
+# @param range_flag The range [ 0, 1, 2, 3 ] to set the system
 def setRange(bus, address, range_flag):
+
+    # Read
     register = 0x31
+
+    # Read old values from the server
     value = bus.read_byte_data(address, register)
 
+    # Wipe the first 16 register values
     value &= ~0x0F;
-    value |= range_flag;
+
+    # Change the range byte1 + byte0
+    value |= (range_flag & 0x03);
+
+    # Enable full resolution
     value |= 0x08;
 
+    # Write to the server
     bus.write_byte_data(address, register, value)
 
+
+##
+# Reads from our accelerometer and returns the exact acceleration values
+# represented in acceleration m/s^2 values in each given axis [ X, Y, Z ]
+#
+# @param bus to read/write
+# @param address of the bus
 def getValue(bus, address):
     # Get 6 bytes from the data register (0x32)
     bytes = bus.read_i2c_block_data(address, 0x32, 6)
@@ -71,17 +90,21 @@ def getValue(bus, address):
     valY = twosToInt(valY, 16);
     valZ = twosToInt(valZ, 16);
 
+    # How many g's am I accelerating, 0.00415 mg seems
+    # to be the most nominal value to get the correct bearings
     valX *= 0.00415
     valY *= 0.00415
     valZ *= 0.00415
 
+    # Given earths gravity what is my acceleration m/s^2
     valX *= 9.80665
     valY *= 9.80665
     valZ *= 9.80665
 
-    valX = round(valX, 4)
-    valY = round(valY, 4)
-    valZ = round(valZ, 4)
+    # Get some rounding going before returning home
+    valX = round(valX, 7)
+    valY = round(valY, 7)
+    valZ = round(valZ, 7)
     
     return [ valX, valY, valZ ]
 
@@ -90,7 +113,7 @@ bus = smbus.SMBus(1)
 address = 0x53
 
 setBandwidthRate(bus, address, 0x0B);
-setRange(bus, address, 0x03);
+setRange(bus, address, 0x01);
 enableMeasurement(bus, address);
 
 # Loop through and get the values
@@ -98,6 +121,9 @@ while True:
     
     (valX, valY, valZ) = getValue(bus, address);
 
-    print "{0: 12f}".format(valX), "{0: 12f}".format(valY), "{0: 12f}".format(valZ)
+    print \
+        "{0: 12f}".format(valX), \
+        "{0: 12f}".format(valY), \
+        "{0: 12f}".format(valZ)
 
     time.sleep(0.04)
